a77aec81b46485334748294986bddf748043b8a0
mohit1523 <85980940+mohit1523@users.noreply.github.com>
2022-12-02 16:07:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,36 @@ let copy = document.querySelector('.copyright');
 let dt = new Date();
 
 copy.innerHTML = `&#169; ${dt.getFullYear()} Copyright : Dopar Energy Ltd.`;
+
+let productImg = document.querySelectorAll('.product-container > div');
+
+document.getElementById('pic1').addEventListener("click" , () => {
+    productImg.forEach(element => {
+        element.style.transform = 'translateX(-30vw)';
+    });
+})
+document.getElementById('pic2').addEventListener("click" , () => {
+    productImg.forEach(element => {
+        element.style.transform = 'translateX(-60vw)';
+    });
+})
+document.getElementById('pic3').addEventListener("click" , () => {
+    productImg.forEach(element => {
+        element.style.transform = 'translateX(0vw)';
+    });
+})
+document.getElementById('pic4').addEventListener("click" , () => {
+    productImg.forEach(element => {
+        element.style.transform = 'translateX(-60vw)';
+    });
+})
+document.getElementById('pic5').addEventListener("click" , () => {
+    productImg.forEach(element => {
+        element.style.transform = 'translateX(0vw)';
+    });
+})
+document.getElementById('pic6').addEventListener("click" , () => {
+    productImg.forEach(element => {
+        element.style.transform = 'translateX(-30vw)';
+    });
+})
