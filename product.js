7b96c3945fea1f5c658aa983afb8b2e27118d2c0
mohit1523 <85980940+mohit1523@users.noreply.github.com>
2022-12-02 12:46:27 +0530

    Update product.js

--- product.js
+++ product.js
@@ -5,7 +5,7 @@ copy.innerHTML = `&#169; ${dt.getFullYear()} Copyright : Dopar Energy Ltd.`;
 
 //CODE FOR PRODUCTS VIEWER
 
-let mainImg = document.querySelector('.product-images > img')
+let mainImg = document.querySelector('.main-img')
 let proBtns = document.querySelectorAll('.product-images > div img');
 
 proBtns.forEach(element => {
@@ -24,5 +24,29 @@ proBtns.forEach(element => {
     element.addEventListener('click' , () => {
         console.log(element);
         mainImg.src = element.src;
+    })
+});
+
+
+let mainImg2 = document.querySelector('.main-img2')
+let proBtns2 = document.querySelectorAll('.product-images2 > div img');
+
+
+proBtns2.forEach(element => {
+    element.addEventListener('mouseenter' , () => {
+        element.style.boxShadow = '5px 5px 10px rgba(0, 0, 0, 0.5)'
+    })
+
+    element.addEventListener('mouseleave' , () => {
+        element.style.boxShadow = 'none'
+    })
+
+    element.addEventListener('mousedown' , () => {
+        element.style.boxShadow = '5px 5px 10px rgba(0, 0, 0, 0.5) inset'
+    })
+
+    element.addEventListener('click' , () => {
+        console.log(element);
+        mainImg2.src = element.src;
     })
 });
