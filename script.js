a171e56444e6f1721ed7d44fff698157237a28ef
mohit1523 <85980940+mohit1523@users.noreply.github.com>
2022-12-17 11:22:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,32 +5,32 @@ copy.innerHTML = `&#169; ${dt.getFullYear()} Copyright : Dopar Energy Ltd.`;
 
 let productImg = document.querySelectorAll('.product-container > div');
 
-document.getElementById('pic1').addEventListener("click" , () => {
+document.getElementById('pic1').addEventListener("click", () => {
     productImg.forEach(element => {
         element.style.transform = 'translateX(-30vw)';
     });
 })
-document.getElementById('pic2').addEventListener("click" , () => {
+document.getElementById('pic2').addEventListener("click", () => {
     productImg.forEach(element => {
         element.style.transform = 'translateX(-60vw)';
     });
 })
-document.getElementById('pic3').addEventListener("click" , () => {
+document.getElementById('pic3').addEventListener("click", () => {
     productImg.forEach(element => {
         element.style.transform = 'translateX(0vw)';
     });
 })
-document.getElementById('pic4').addEventListener("click" , () => {
+document.getElementById('pic4').addEventListener("click", () => {
     productImg.forEach(element => {
         element.style.transform = 'translateX(-60vw)';
     });
 })
-document.getElementById('pic5').addEventListener("click" , () => {
+document.getElementById('pic5').addEventListener("click", () => {
     productImg.forEach(element => {
         element.style.transform = 'translateX(0vw)';
     });
 })
-document.getElementById('pic6').addEventListener("click" , () => {
+document.getElementById('pic6').addEventListener("click", () => {
     productImg.forEach(element => {
         element.style.transform = 'translateX(-30vw)';
     });
@@ -41,9 +41,19 @@ document.getElementById('pic6').addEventListener("click" , () => {
 
 let mainImg = document.querySelector('.main-img')
 let proBtns = document.querySelectorAll('.product-images > div img');
+let animation1 = [{ opacity: 1 }, { opacity: 0 }];
+let animation2 = [{ opacity: 0 }, { opacity: 1 }];
+let animeSet = { duration: 400, iterations: 1 };
+
+let currImgSrc = mainImg.src;
 
 proBtns.forEach(element => {
-    element.addEventListener('click' , () => {
-        mainImg.src = element.src;
+    element.addEventListener('click', () => {
+        if (currImgSrc != element.src) {
+            mainImg.animate(animation1, animeSet);
+            mainImg.src = element.src;
+            mainImg.animate(animation2, animeSet);
+        }
+        currImgSrc = element.src;
     })
 });
